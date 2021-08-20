efaefcc80a9b6153ee1f19c2a0bfe250dfd4d953
Tierra <tierrarucker@ymail.com>
2021-08-20 06:27:45 -0400

    setup and basic layout

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/* 
+// Project: A todo list app
+// Step 1: type in input field, click button or press ENTER to add item
+// Step 2: click the trash can and 
+// Step 
+// BONUS 1: edit the item. Change the name 
+// BONUS 2: edit the item. Change the quantity
+*/
+
+
+// elements 
+inputEl = document.getElementsByTagName('input');
+deleteBtn = document.getElementsByClassName('fa-trash');
+
+
+// functions 
+
+function addItem(){
+  // console.log('add item');
+  item = document.createElement('li');
+  li.appendChild(item);
+}
+
+function removeItem(){
+  console.log('remove item');
+}
+
+function clearTasks(){
+
+}
+
+
+/* 
+// bonus: 
+
+*/
+// console.log(inputEl);
